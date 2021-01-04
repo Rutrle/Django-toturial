bec24f04f408d0561dfd3c6aca134785b2484350
Jakub <rutrle@seznam.cz>
2021-01-04 01:31:35 +0100

    better way to render from database

--- src/trydjango/products/views.py
+++ src/trydjango/products/views.py
@@ -3,8 +3,14 @@ from .models import Product
 # Create your views here.
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
+    
+    '''inefficient way
     context = {
         'title':obj.title,
         'description':obj.description,
+    }
+    '''
+    context = {
+        'object':obj
     }
     return render(request, "product/detail.html", context)
