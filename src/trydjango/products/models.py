ebebf0d9fc419aa4f8814527bb9d0ab1ee82852f
Jakub <rutrle@seznam.cz>
2021-01-05 02:38:15 +0100

    form

--- src/trydjango/products/models.py
+++ src/trydjango/products/models.py
@@ -6,4 +6,4 @@ class Product(models.Model):
     description = models.TextField(blank = True, null = True) #null = True - makes the vallue optional, 
     price = models.DecimalField(decimal_places = 2, max_digits = 10000)
     summary = models.TextField(blank=True, null=False)#blank deals with how field is rendered when filling it in (optional/mandatory), null deals with database - if this value muset be there
-    featured = models.BooleanField() # new field, possibilities: 1) delete database and make new one, 2) null = True -> in all old data this field will be empty 3) default = True => sets default value  to the value given 4) give info to python during  making migrations
+    featured = models.BooleanField(default = False) # new field, possibilities: 1) delete database and make new one, 2) null = True -> in all old data this field will be empty 3) default = True => sets default value  to the value given 4) give info to python during  making migrations
--- src/trydjango/trydjango/urls.py
+++ src/trydjango/trydjango/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, about_view, social_view #better to import specific view
-from products.views import product_detail_view
+from products.views import product_detail_view, product_create_view
 
 urlpatterns = [
     path('', home_view, name = 'home'),
@@ -25,5 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', about_view),
     path('social/', social_view),
-    path('product/',product_detail_view)
+    path('product/',product_detail_view),
+    path('create/',product_create_view)
 ]
