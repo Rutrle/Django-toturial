205087623ebad8671cb9af76e1f913ef43495f54
Jakub <rutrle@seznam.cz>
2021-01-06 03:13:25 +0100

    handling exceptions

--- src/trydjango/products/views.py
+++ src/trydjango/products/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 from .forms import ProductForm, RawProductForm
 # Create your views here.
 
 def dynamic_lookup_view(request, my_id):
-    obj = Product.objects.get(id=my_id)
+    #obj = Product.objects.get(id=my_id)
+    #obj = get_object_or_404(Product, id = my_id) => one of possibilities
+    try:
+        obj = Product.objects.get(id=my_id)
+    except Product.DoesNotExist:
+        raise Http404
+        
     context={
         "object":obj
 
