ec20762e059f3dfb0daab4b2e3bdb7d499ea91d8
Jakub <rutrle@seznam.cz>
2021-01-06 03:09:11 +0100

    dynamic lookup field

--- src/trydjango/trydjango/urls.py
+++ src/trydjango/trydjango/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, about_view, social_view #better to import specific view
-from products.views import product_detail_view, product_create_view, render_initial_data
+from products.views import product_detail_view, product_create_view, render_initial_data, dynamic_lookup_view
 
 urlpatterns = [
     path('', home_view, name = 'home'),
@@ -27,5 +27,6 @@ urlpatterns = [
     path('social/', social_view),
     path('product/',product_detail_view),
     path('create/',product_create_view),
-    path('initial/',render_initial_data)
+    path('initial/',render_initial_data),
+    path('products/<int:my_id>/', dynamic_lookup_view, name = 'product'),
 ]
