0459fae347b758f7b4addab0e0e8dc0d2aae8ef7
Jakub <rutrle@seznam.cz>
2021-09-25 00:35:30 +0200

    basic graphs with temperatures in detail view

--- src/trydjango/Blog/views.py
+++ src/trydjango/Blog/views.py
@@ -158,6 +158,8 @@ class CourseCreateView(View):
         form = CourseModelForm(request.POST)
         if form.is_valid():
             form.save()
+            form = CourseModelForm()
+
         context = {"form": form}
 
         return render(request, self.template_name, context)
